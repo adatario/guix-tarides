28d60dcce9ddc90869098beb1fcd36f43cb10946
Adarsh Amirtham <adarsh@tarides.com>
2022-11-18 15:36:04 +0100

    tezos: Add ocaml-tezos-stdlib.

--- tezos.scm
+++ tezos.scm
@@ -8,7 +8,9 @@
   #:use-module (guix build-system dune)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages ocaml)
-  #:use-module (gnu packages pkg-config))
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages multiprecision)
+  #:use-module (tarides ocaml))
 
 (define-public ocaml-tezos-base58
   (package
@@ -31,3 +33,42 @@
    (synopsis "Base58 encoding for Tezos")
    (description "Self-contained package for base58 encoding used by Tezos.")
    (license license:expat)))
+
+(define tezos
+  (package
+   (name "tezos")
+   (version "15.0")
+   (home-page "https://gitlab.com/tezos/tezos")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+	   (url (string-append home-page ".git"))
+	   (commit (string-append "v" version))))
+     (sha256
+      (base32
+       "0jmzgw4ay1kjywhq8ygbndddfgwm05w2zki853wx04vdgd9i1zi9"))))
+   (build-system dune-build-system)
+   (synopsis "Tezos")
+   (description "Tezos")
+   (license license:expat)))
+
+(define-public ocaml-tezos-stdlib
+  (package
+   (inherit tezos)
+   (name "ocaml-tezos-stdlib")
+   (propagated-inputs
+    (list ocaml-lwt
+	  ocaml-hex
+	  ocaml-zarith
+	  ocaml-zarith-stubs-js
+	  ocaml-ppx-expect
+	  ocaml-ringo-0.9
+	  gmp))
+   (native-inputs
+    (list ocaml-alcotest
+	  ocaml-qcheck))
+   (arguments `(#:package "tezos-stdlib"
+		#:test-target "."
+		;; TODO package tezos-test-helpers for tests
+		#:tests? #f))))
